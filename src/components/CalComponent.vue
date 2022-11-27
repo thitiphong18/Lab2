30369b6f4c370fe51d165c2f73a1a1ce96bab18d
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-27 23:41:29 +0700

    Update 2

---
+++ src/components/CalComponent.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { reactive,computed} from 'vue';
+export default {
+    setup() {
+        const state = reactive({
+            num1: '0',
+            num2: '0',
+            result: 0
+        });
+        const result = computed(function () {
+            return parseInt(state.num1) + parseInt(state.num2)
+        });
+        function add() {
+            state.result = parseInt(state.num1) + parseInt(state.num2)
+        }
+        return { state, add, result };
+    },
+};
+</script>
+
+
+
+<template>
+    <div class="card">
+        <div>
+            <input v-modul="state.num1" typr="text" ref="num1" style="text-align: center; width : 80%" />
+        </div>
+    </div>
+    <div>+</div>
+    <div>
+        <input v-modul="state.num2" typr="text" ref="num2" style="text-align: center; width : 80%" />
+    </div>
+    <hr />
+    <button @click="add" stlye="width: 80%">=</button>
+    <div ref="result">{{ state.num1 }}+{{ state.num2 }} = {{ result }}</div>
+    </div>
+
+</template>
+
+<style>
+
+</style>
