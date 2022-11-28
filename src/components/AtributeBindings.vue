cc302aa8b994ee7ccc2919ee08dc7e38b620cdb3
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-28 14:04:09 +0700

    Update 6

---
+++ src/components/AtributeBindings.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+<template>
+  <p>
+    <span title="message">
+      Hover your over me for second to see my dinamically bound titled!
+    </span>
+  </p>
+
+  <p class="red">
+    <span> This should be red... but click me toggle it. </span>
+  </p>
+
+  <p style="color: green">
+    <span> This should be red... but click me toggle it. </span>
+  </p>
+</template>
+
+<style scoped></style>
