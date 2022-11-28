9dbf5e823a5a4f506eca3e2c8dfba3e5d7c56fc9
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-28 20:39:38 +0700

    Update 8.2

--- src/components/FormBindings.vue
+++ src/components/FormBindings.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 const text = ref("Edit Me");
 const checked = ref(true);
+const checkedNames = ref(["Jack"]);
 </script>
 
 <template>
@@ -11,6 +12,16 @@ const checked = ref(true);
   <h2>Checkbox</h2>
   <input type="checkbox" v-model="checked" id="checkbox" />
   <label for="checkbox">Checked {{ checked }}</label>
+
+  <h2>Multiply Checkbox</h2>
+  <input type="checkbox" v-model="checkedNames" id="jack" value="Jack" />
+  <label for="jack">Jack</label>
+  <input type="checkbox" v-model="checkedNames" id="john" value="John" />
+  <label for="john">John</label>
+  <input type="checkbox" v-model="checkedNames" id="mike" value="Mike" />
+  <label for="mike">Mike</label>
+  <p>Check names</p>
+  <pre> {{ checkedNames }} </pre>
 </template>
 
 <style scoped></style>
