f7a200df0beec9bb9d084ddac0c7bd0529505fce
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-28 20:30:23 +0700

    Update 8.1

--- src/components/FormBindings.vue
+++ src/components/FormBindings.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { ref } from "vue";
 const text = ref("Edit Me");
+const checked = ref(true);
 </script>
 
 <template>
   <h2>Text Input</h2>
   <input v-model="text" /> {{ text }}
+
+  <h2>Checkbox</h2>
+  <input type="checkbox" v-model="checked" id="checkbox" />
+  <label for="checkbox">Checked {{ checked }}</label>
 </template>
 
 <style scoped></style>
