c42c5d9c409221b72a96222ef16502ac7da9c5eb
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-28 13:18:28 +0700

    Update 5

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@ import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import CalComponent from "./components/CalComponent.vue";
 import HelloWorldExample from "./components/HelloWorldExample.vue";
+import HandingUserInput from "./components/HandingUserInput.vue";
 </script>
 
 <template>
   <div>
+    <HandingUserInput />
     <CalComponent />
     <HelloWorldExample />
   </div>
---
+++ src/components/HandingUserInput.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Hello World</h1>
+  <button>Reverse message</button>
+  <button>Append "!"</button>
+  <a href="">A link with e.preventDefault </a>
+</template>
+
+<style scoped>
+button,
+a {
+  display: block;
+  margin-bottom: 1em;
+}
+</style>
