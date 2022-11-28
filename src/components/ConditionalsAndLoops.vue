4a3efcbbf8785b08414d59b601b290cb791cffcf
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-11-28 20:06:02 +0700

    Update 7.4

--- src/components/ConditionalsAndLoops.vue
+++ src/components/ConditionalsAndLoops.vue
@@ -6,11 +6,11 @@ const list = ref([1, 2, 3]);
 
 <template>
   <button @click="show = !show">Toggle List</button>
-  <button>Push Number</button>
-  <button>Pop number</button>
-  <button>Reverse List</button>
+  <button @click="list.push(list.length + 1)">Push Number</button>
+  <button @click="list.pop()">Pop number</button>
+  <button @click="list = list.reverse()">Reverse List</button>
 
-  <ul v-if="show">
+  <ul v-if="show && list.length">
     <li v-for="(item, index) of list" :key="index">{{ item }}</li>
   </ul>
   <p v-else-if="list.length">List is not empty, but hidden.</p>
